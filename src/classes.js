3cb360dfdc1a19ae1cde284d39ba6e4aaa2493d8
KVG24 <141866132+KVG24@users.noreply.github.com>
2025-01-21 12:39:08 +0000

    Create and test Gameboard class

--- src/classes.js
+++ src/classes.js
@@ -15,3 +15,79 @@ export class Ship {
         return this.hitNumber == this.length;
     }
 }
+
+export class Gameboard {
+    constructor() {
+        this.missedAttacks = [];
+        this.ships = [];
+        this.board = Array(10)
+            .fill(null)
+            .map(() => Array(10).fill(null));
+    }
+
+    placeShip(ship, startX, startY, direction) {
+        if (!this.isPlacementValid(ship, startX, startY, direction)) {
+            throw new Error("Invalid coordinates");
+        }
+
+        const coordinates = [];
+        for (let i = 0; i < ship.length; i++) {
+            if (direction === "horizontal") {
+                this.board[startX][startY + i] = ship;
+                coordinates.push([startX, startY + i]);
+            } else if (direction === "vertical") {
+                this.board[startX + i][startY] = ship;
+                coordinates.push([startX + i, startY]);
+            }
+        }
+
+        this.ships.push({ ship, coordinates });
+    }
+
+    isPlacementValid(ship, startX, startY, direction) {
+        if (direction === "horizontal") {
+            if (startY + ship.length > 10) {
+                return false;
+            }
+        } else if (direction === "vertical") {
+            if (startX + ship.length > 10) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        for (let i = 0; i < ship.length; i++) {
+            let x, y;
+            if (direction === "horizontal") {
+                x = startX;
+                y = startY + i;
+            } else {
+                x = startX + i;
+                y = startY;
+            }
+
+            if (this.board[x][y] !== null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    receiveAttack(x, y) {
+        const target = this.board[x][y];
+
+        if (target === null) {
+            this.missedAttacks.push([x, y]);
+            this.board[x][y] = "miss";
+        } else if (typeof target === "object" && target instanceof Ship) {
+            target.hit();
+            this.board[x][y] = "hit";
+        }
+    }
+
+    allShipsSunk() {
+        return this.ships.every(({ ship }) => ship.isSunk());
+    }
+}
--- src/classes.test.js
+++ src/classes.test.js
@@ -1,4 +1,4 @@
-import { Ship } from "./classes";
+import { Ship, Gameboard } from "./classes";
 
 describe("Ship class", () => {
     let ship;
@@ -39,5 +39,52 @@ describe("Ship class", () => {
         ship.hit();
         ship.hit();
         expect(ship.hit()).toBe(false);
+    });
+});
+
+describe("Gameboard class", () => {
+    let gameboard;
+    let ship;
+
+    beforeEach(() => {
+        gameboard = new Gameboard();
+        ship = new Ship("Submarine", 3);
+    });
+
+    test("Placing ship on gameboard", () => {
+        gameboard.placeShip(ship, 3, 4, "horizontal");
+        expect(gameboard.board[3][4]).toBe(ship);
+        expect(gameboard.board[3][5]).toBe(ship);
+        expect(gameboard.board[3][6]).toBe(ship);
+    });
+
+    test("Receive attack and miss", () => {
+        gameboard.placeShip(ship, 2, 2, "vertical");
+        gameboard.receiveAttack(2, 3);
+        expect(gameboard.board[2][3]).toBe("miss");
+        expect(gameboard.missedAttacks).toContainEqual([2, 3]);
+        expect(ship.hitNumber).toBe(0);
+    });
+
+    test("Receive attack on ship", () => {
+        gameboard.placeShip(ship, 5, 1, "horizontal");
+        gameboard.receiveAttack(5, 2);
+        expect(ship.hitNumber).toBe(1);
+    });
+
+    test("Destroy ship", () => {
+        gameboard.placeShip(ship, 2, 5, "vertical");
+        gameboard.receiveAttack(2, 5);
+        gameboard.receiveAttack(3, 5);
+        gameboard.receiveAttack(4, 5);
+        expect(ship.isSunk()).toBeTruthy();
+    });
+
+    test("All ships on the board is destroyed", () => {
+        gameboard.placeShip(ship, 8, 5, "horizontal");
+        gameboard.receiveAttack(8, 5);
+        gameboard.receiveAttack(8, 6);
+        gameboard.receiveAttack(8, 7);
+        expect(gameboard.allShipsSunk()).toBeTruthy();
     });
 });
