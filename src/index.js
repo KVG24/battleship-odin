ffae1667d357eee2d9d2ae237e9b04fd328690bf
KVG24 <141866132+KVG24@users.noreply.github.com>
2025-01-31 08:39:18 +0000

    Change cell color on hover

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { newGame } from "./logic";
 newGame();
 
 // To Do:
-// - make cells that being hit more visible
 // - make preview of placed ship on preview gameboard
 // - add smart computer moves
 // - clean up all js code, add separate DOM handling module
